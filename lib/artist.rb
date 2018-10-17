6c03dba8eefcae051289b3d72e388c1b391aaacf
Edgar Irizarry <eirizarry15@yahoo.com>
2018-10-17 17:59:25 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,9 +20,48 @@ class artist
   end 
   
   def self.find(name)
-    artisit = self.new(name)
-    artist.name = name 
-    artist 
+    self.all.detect { |artist| artist.name == name}  
   end 
+  
+  def self.find_or_create_by_name(name)
+    self.find(name) || self.create(name)
+  end 
+  
+  def add_song(song)
+    @songs << song 
+  end 
+  
+  def print_songs 
+    songs.each { |song| puts song.name }
+  end 
+  
+  def save
+    @@all << self 
+  end 
+  
+  def songs 
+    @songs 
+  end 
+  
+end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   
